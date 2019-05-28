9b62b5031ce0f658abbfb750fc448af44b0fa1f9
Mati Horovitz <mati@pojo.me>
2019-05-28 16:32:24 +0300

    New: Add Review - Send notifications..

--- integrations/zimmer-review.php
+++ integrations/zimmer-review.php
@@ -1,6 +1,7 @@
 <?php
 namespace WhiteZimmer\Integrations;
 
+use Elementor\Controls_Manager;
 use ElementorPro\Modules\Forms\Classes\Action_Base;
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -17,7 +18,34 @@ class Zimmer_Review extends Action_Base {
 		return 'Zimmer Review';
 	}
 
-	public function register_settings_section( $widget ) {}
+	public function register_settings_section( $widget ) {
+		$widget->start_controls_section(
+			'section_review',
+			[
+				'label' => __( 'Review', 'elementor-pro' ),
+				'condition' => [
+					'submit_actions' => $this->get_name(),
+				],
+			]
+		);
+
+		$widget->add_control(
+			'notification_to',
+			[
+				'label' => __( 'Notification To', 'elementor-pro' ),
+				'type' => Controls_Manager::SELECT2,
+				'multiple' => true,
+				'options' => [
+					'owner' => 'Owner',
+					'reviewer' => 'Reviewer',
+					'site_admin' => 'Site Admin',
+				],
+				'render_type' => 'none',
+			]
+		);
+
+		$widget->end_controls_section();
+	}
 
 	public function on_export( $element ) {}
 
@@ -36,5 +64,18 @@ class Zimmer_Review extends Action_Base {
 		foreach ( $data as $key => $field ) {
 			add_post_meta( $review_id, $key, $field['value'] );
 		}
+
+		$this->send_notification( $record );
+	}
+
+	private function send_notification( $record ) {
+		$notification_to = $record->get_form_settings( 'notification_to' );
+
+		if ( ! empty( $notification_to ) ) {
+			return;
+		}
+
+		$data = $record->get( 'fields' );
+		$zimmer_id = $data['zimmer_id']['value'];
 	}
 }
